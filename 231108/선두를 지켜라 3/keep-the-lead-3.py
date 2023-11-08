34999f4bd7aaf981e1c017f4c81d4be0fab11f36
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2023-11-08 10:18:34 +0900

    Title: 선두를 지켜라 3, Time: 213ms, Memory: 43MB, Status: Passed - CodeTree

--- 231108/선두를 지켜라 3/keep-the-lead-3.py
+++ 231108/선두를 지켜라 3/keep-the-lead-3.py
@@ -1,17 +1,20 @@
-n,m = map(int,input().split())
+n, m = map(int, input().split())
 
 time_a = [0]
-for i in range(1,n+1):
-    v,t = map(int,input().split())
+a_now = 0
+for i in range(1, n + 1):
+    v, t = map(int, input().split())
     for j in range(t):
-        time_a.append(time_a[j-1] + v)
+        a_now += 1
+        time_a.append(time_a[a_now - 1] + v)
 
 time_b = [0]
-for i in range(1,m+1):
-    v,t = map(int,input().split())
+b_now = 0
+for i in range(1, m + 1):
+    v, t = map(int, input().split())
     for j in range(t):
-        time_b.append(time_b[j-1] + v)
-
+        b_now += 1
+        time_b.append(time_b[b_now - 1] + v)
 
 # 길이(전체 시간) 맞추기
 if len(time_a) >= len(time_b):
@@ -26,13 +29,13 @@ lower_last = lower[-1]
 for _ in range(gap):
     lower.append(lower_last)
 
-# print(time_a)
-# print(time_b)
 
 head = 0
 cnt = 0
-for i in range(len(longer)):
-    
+# print(time_a)
+# print(time_b)
+for i in range(1,len(longer)):
+    # print(head,cnt,i)
     if time_a[i] > time_b[i]:
         if head == 'a': continue
         head = 'a'
@@ -41,6 +44,11 @@ for i in range(len(longer)):
     elif time_a[i] < time_b[i]:
         if head == 'b': continue
         head = 'b'
+        cnt += 1
+
+    elif time_a[i] == time_b[i]:
+        if head == 'ab': continue
+        head = 'ab'
         cnt += 1
 
 print(cnt)
