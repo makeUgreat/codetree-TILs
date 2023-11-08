e3c9a35c4d34be7e2698d6d9c14f580671606e5f
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2023-11-08 09:37:01 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 231108/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 231108/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,33 +1,33 @@
-N,K,P,T = map(int,input().split())
+N, K, P, T = map(int, input().split())
 
-infection = [0] * (N+1)
+infection = [0] * (N + 1)
 infection[P] = 1
 
-time = [[0,0] for _ in range(251)]
-cnt_check = [0] * (N+1)
+time = [[0, 0] for _ in range(251)]
+cnt_check = [0] * (N + 1)
 for _ in range(T):
-    t,x,y = map(int,input().split())
-    time[t] = [x,y]
-     
+    t, x, y = map(int, input().split())
+    time[t] = [x, y]
 
+for a, b in time:
+    if a == 0 and b == 0: continue
 
-for a,b in time:
-    if a==0 and b==0: continue
-    
     if infection[a] == 1 and infection[b] == 0:
         if cnt_check[a] < K:
             cnt_check[a] += 1
             infection[b] = 1
-            cnt_check[b] += 1
-    
+            # cnt_check[b] += 1
+
     elif infection[a] == 0 and infection[b] == 1:
         if cnt_check[b] < K:
             cnt_check[b] += 1
             infection[a] = 1
-            cnt_check[a] += 1
-    
+            # cnt_check[a] += 1
+
     elif infection[a] == 1 and infection[b] == 1:
         cnt_check[a] += 1
         cnt_check[b] += 1
-    
-print(*infection[1:],sep='')
+
+    # print(a,b,infection[1:])
+
+print(*infection[1:], sep='')
