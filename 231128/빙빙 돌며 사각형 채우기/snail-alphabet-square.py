eb4e56676db0c15cefd48ee7aaaf154e2da6285b
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2023-11-28 12:58:30 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 203ms, Memory: 25MB, Status: Passed - CodeTree

--- 231128/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
+++ 231128/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -1,29 +1,29 @@
-n,m = map(int,input().split())
+n, m = map(int, input().split())
 
-grid = [[0]*m for _ in range(n)]
+grid = [[0] * m for _ in range(n)]
 grid[0][0] = 'A'
-y,x = 0,0
+y, x = 0, 0
 
-diy = [0,1,0,-1]
-dix = [1,0,-1,0]
+diy = [0, 1, 0, -1]
+dix = [1, 0, -1, 0]
 idx = 0
-cnt = ord('A')
-
-while cnt < ord('A')+(n*m)-1:
-
+cnt = 0
+t = 0
+while t < n*m-1:
     dy = y + diy[idx]
     dx = x + dix[idx]
-    if dy<0 or dx<0 or dy>=n or dx>=m:
-        idx = (idx+1)%4
+    if dy < 0 or dx < 0 or dy >= n or dx >= m:
+        idx = (idx + 1) % 4
         continue
     if grid[dy][dx] != 0:
-        idx = (idx+1)%4
+        idx = (idx + 1) % 4
         continue
 
-    y,x = dy,dx
+    y, x = dy, dx
 
-    cnt += 1
-    grid[y][x] = chr(cnt)
+    cnt = (cnt + 1) % 26
+    grid[y][x] = chr(cnt + ord('A'))
+    t += 1
 
 for g in grid:
     print(*g)
