f694a8fc092af2d6a7ffd29536fd34d20903969b
makeUgreat <137480733+makeUgreat@users.noreply.github.com>
2023-12-05 16:00:54 +0900

    Title: 일렬로 서있는 소 2, Time: 167ms, Memory: 28MB, Status: Passed - CodeTree

--- 231205/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 231205/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -9,7 +9,7 @@ cnt = 0
 for p in permutation:
 
     if p[0] < p[1] < p[2]:
-        if cows[p[0]] < cows[p[1]] < cows[p[2]]:
+        if cows[p[0]] <= cows[p[1]] <= cows[p[2]]:
             cnt += 1
 
 print(cnt)
